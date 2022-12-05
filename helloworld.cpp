f59585b882d75bf4b8bb48b9ac59a26b1ef44a3f
zhany-bek <daveelfie@gmail.com>
2022-12-05 19:03:04 +0600

    Do practice until hypotenuse calculation (incl.)

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-using namespace std;
+#include <cmath>
 
 int main() {
-    const double PI = 3.14159;
-    double radius = 10;
-    double circumference = 2 * PI * radius;
-    cout << circumference << " cm";
+    double a;
+    double b;
+    double c;
+
+    std::cout << "Enter side a: ";
+    std::cin >> a;
+    std::cout << "Enter side b: ";
+    std::cin >> b;
+
+    c = sqrt(pow(a, 2) + pow(b, 2));
+    std::cout << "Hypotenuse is equal to " << c;
+
     return 0;
 }
