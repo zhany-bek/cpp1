2107112e555926dbbc44982fe5850e4bb38c9600
Zhanybek Bekbolat <105671800+zhany-bek@users.noreply.github.com>
2022-12-05 17:44:52 +0600

    Add existing files

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+    cout << circumference << " cm";
+    return 0;
+}
